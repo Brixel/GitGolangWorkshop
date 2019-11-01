52beaff0d24016e7e011f3abc08776f4e19b9432
wim van laer <van.laer.wim@gmail.com>
2019-11-01 15:58:39 +0100

    Created direction

---
+++ direction.go
@@ -0,0 +1,39 @@
+package maze
+
+// Direction is an int enum that represents a direction in a maze
+// (North, East, South and West)
+type Direction int
+
+const (
+	North Direction = iota
+	East
+	South
+	West
+)
+
+// Directions is a slice of maze.Direction.
+type Directions []Direction
+
+var (
+	// PossibleDirections is a Directions that contains all the possible
+	// directions the enum can have.
+	PossibleDirections Directions
+)
+
+func init() {
+	PossibleDirections = make(Directions, 4, 4)
+	PossibleDirections[North] = North
+	PossibleDirections[East] = East
+	PossibleDirections[South] = South
+	PossibleDirections[West] = West
+}
+
+// Contains returns whether Directions contains a given direction.
+func (ds Directions) Contains(find Direction) bool {
+	return false
+}
+
+// String formats the Direction. It returns the name of the enum value.
+func (d Direction) String() string {
+	return "TODO: format direction"
+}
